dcbbc5684f3056931bf1643a50012616e65cc286
guga56 <gustavogregorio086@gmail.com>
2023-05-29 14:31:36 -0300

    inicializar o projeto

---
+++ ProjetoEstucoCompra.Entidade/Entidade/Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoEstucoCompra.Entidade.Entidade
+{
+    [Table("Login")]
+    public class Login
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int LoginId { get; set; }
+
+        public string Usuario { get; set; }
+
+        public string Senha { get; set; }
+
+        public string Email { get; set; }
+
+        public string Perfil { get; set; }
+    }
+}
---
+++ ProjetoEstudoCompra.Data/Persistence/ProjetoEstudoCompraContexto.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoEstucoCompra.Entidade.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEstudoCompra.Data.Persistence
+{
+    public class ProjetoEstudoCompraContexto : DbContext
+    {
+        public ProjetoEstudoCompraContexto()
+        {
+
+        }
+
+        public ProjetoEstudoCompraContexto(DbContextOptions option)
+            : base(option)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Login>().ToTable("Login");
+            modelBuilder.Entity<Login>().HasKey(l => l.LoginId).IsClustered();
+            modelBuilder.Entity<Login>().Property(l => l.Usuario).IsRequired();
+            modelBuilder.Entity<Login>().Property(l => l.Senha).IsRequired();
+            modelBuilder.Entity<Login>().Property(l => l.Perfil).IsRequired();
+            modelBuilder.Entity<Login>().Property(l => l.Email).IsRequired();
+
+        }
+
+        public DbSet<Login> logins { get; set; }
+    }
+}
