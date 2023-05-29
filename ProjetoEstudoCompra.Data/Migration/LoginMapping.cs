aa3dd4ddee92373286aeddc3b8ceab890346d943
guga56 <gustavogregorio086@gmail.com>
2023-05-29 16:37:37 -0300

    estruturando e organizando melhor o projeto

---
+++ ProjetoEstudoCompra.Data/Migration/LoginMapping.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoEstucoCompra.Entidade.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEstudoCompra.Data.Migration
+{
+    public class LoginMapping
+    {
+        public void OnModelCreate(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Login>().ToTable("Login");
+            modelBuilder.Entity<Login>().HasKey(l => l.LoginId);
+            modelBuilder.Entity<Login>().Property(l => l.Usuario).IsRequired();
+            modelBuilder.Entity<Login>().Property(l => l.Senha).IsRequired();
+            modelBuilder.Entity<Login>().Property(l => l.Perfil).IsRequired();
+            modelBuilder.Entity<Login>().Property(l => l.Email).IsRequired();
+
+        }
+    }
+}
