c8efe7fccc55d7261c43d36143c8209dc8f37a1c
guga56 <gustavogregorio086@gmail.com>
2023-05-29 14:42:33 -0300

    colocar a configuração no startup

--- ProjetoEstudoCompra/Startup.cs
+++ ProjetoEstudoCompra/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProjetoEstudoCompra.Data.Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,7 @@ namespace ProjetoEstudoCompra
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ProjetoEstudoCompraContexto>(opts => opts.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
