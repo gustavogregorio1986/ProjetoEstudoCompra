a374a53ff101a3307df0412f0f19bf56f54162ff
guga56 <gustavogregorio086@gmail.com>
2023-06-05 11:51:48 -0300

    criar o controller e a view

--- ProjetoEstudoCompra/Controllers/LoginController.cs
+++ ProjetoEstudoCompra/Controllers/LoginController.cs
@@ -1,14 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoEstucoCompra.Entidade.Entidade;
+using ProjetoEstudoCompra.Models;
+using ProjetoEstudoCompra.Servico.Servico;
 
 namespace ProjetoEstudoCompra.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly LoginServico loginServico;
+
+        public LoginController()
+        {
+            loginServico = new LoginServico();
+        }
+
         public IActionResult Cadastrar()
         {
             return View();
         }
 
-        
+        public IActionResult CadastrarLogin(LoginView loginView)
+        {
+            loginView = new LoginView();
+            Login login = new Login();
+            loginView.Usuario = login.Usuario;
+            loginView.Senha = login.Senha;
+            loginView.Email = login.Email;
+            loginView.Perfil = login.Perfil;
+            loginServico.Cadastrar(login);
+            return View(login);
+        }
     }
 }
